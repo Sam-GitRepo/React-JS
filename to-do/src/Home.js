95bebff128b95037eb1df636ffd3802a8524105f
Sam <smarttsamy@gmail.com>
2023-04-27 19:53:48 +0530

    use state

--- to-do/src/Home.js
+++ to-do/src/Home.js
@@ -1,9 +1,21 @@
+import {useState} from 'react'
+
 const Home = () => {
-    return ( 
+    const [name, setName] = useState('sam');
+    const [age, setAge] = useState(25);
+
+    const handleClick = (e) => {
+        setName('hritik');
+        setAge(24);
+    }
+
+    return (
         <div className="home">
             <h2>HomePage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>click me</button>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
