be40d2c0716a997b86ebe3bf63d7492efa0886b4
Sam <smarttsamy@gmail.com>
2023-05-08 20:12:40 +0530

    Blogs

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -3,7 +3,6 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
-import NotFound from './NotFound';
 
 function App() {
   return (
@@ -20,9 +19,6 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
-            </Route>
-            <Route path="*">
-              <NotFound />
             </Route>
           </switch>
         </div>
